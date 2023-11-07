51fa4c8dee783c877e33ded7ba1b0645dc4ab3e5
Novaes07 <107013838+Novaes07@users.noreply.github.com>
2023-11-07 20:09:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomPom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeBotoes = Document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeBotoes.length){
+    listaDeBotoes[contador].onclick = tocaSom;
+    contador = contador + 1;
 }
